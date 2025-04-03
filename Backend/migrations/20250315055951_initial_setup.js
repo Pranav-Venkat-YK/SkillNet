6f38f55a6907e5063b30665b06b1d54bbcfb2cc1
ryashwanth990 <ryashwanth990@gmail.com>
2025-04-03 13:19:31 +0530

    Update 20250315055951_initial_setup.js

--- Backend/migrations/20250315055951_initial_setup.js
+++ Backend/migrations/20250315055951_initial_setup.js
@@ -71,10 +71,42 @@ exports.up = function (knex) {
             table.decimal("postdegree_grade", 5, 2); 
             table.string("postdegree_course");
             table.string("postdegree_university");
+        })
+          .createTable("jobs", function (table) {
+            table.increments("job_id").primary();
+            table.integer("id").unsigned().references("id").inTable("organisations").notNullable();
+            table.string("title").notNullable();
+            table.text("description").notNullable();
+            table.text("requirements");
+            table.text("responsibilities");
+            table.string("location");
+            table.boolean("is_remote");
+            table.decimal("salary_min", 10, 2);
+            table.decimal("salary_max", 10, 2);
+            table.string("salary_currency");
+            table.enu("employment_type", ["full-time", "part-time"]);
+            table.string("experience_level");
+            table.enu("education_level", ["12th", "UnderGraduate", "PostGraduate", "PhD"]);
+            table.string("domain_of_study");
+            table.date("application_deadline");
+            table.enu("status", ["open", "closed"]).defaultTo("open");
+            table.integer("views_count").defaultTo(0);
+            table.integer("applications_count").defaultTo(0);
+            table.timestamps(true, true);
+        })
+           .createTable("applications", function (table) {
+            table.increments("application_id").primary();
+            table.integer("job_id").unsigned().references("job_id").inTable("jobs").notNullable();
+            table.integer("user_id").unsigned().references("id").inTable("users").notNullable();
+            table.string("resume_url");
+            table.enu("status", ["applied", "viewed","shortlisted", "interviewing", "hired", "rejected"]).defaultTo("applied");
+            table.timestamp("applied_at").defaultTo(knex.fn.now());
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+            table.unique(["job_id", "user_id"]);
         });
         
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTableIfExists("education").dropTableIfExists("workingprofessional").dropTableIfExists("student").dropTableIfExists("organisations").dropTableIfExists("users");
+    return knex.schema.dropTableIfExists("applications") .dropTableIfExists("jobs").dropTableIfExists("education").dropTableIfExists("workingprofessional").dropTableIfExists("student").dropTableIfExists("organisations").dropTableIfExists("users");
 };
