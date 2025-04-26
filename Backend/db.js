f9ae6661fed8119e4dd16b8cf474a44c1a88c961
Mithun Patil <mithunpatil2005@gmail.com>
2025-04-27 00:40:48 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -4,9 +4,9 @@ const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1", // or "localhost"
-    user: "myuser",
-    password: "password", // Update with your actual password
-    database: "mydatabase",
+    user: "your_username_here",
+    password: "your_password_here", // Update with your actual password
+    database: "your_database_here",
   },
 });
 
